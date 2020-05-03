b9df439570f21379bf87e54fbbb386f32de1ea34
JeongHM <kjhm0607@gmail.com>
2020-05-04 02:03:04 +0900

    [Feat] User 회원가입 컨트롤러 구현

--- controllers/user.py
+++ controllers/user.py
@@ -1,28 +1,60 @@
 from flask import Blueprint, request
 
+from utils.decorators import response_format
+from utils.response_code import RESPONSE_CODE
+from services.internals.user import UserService
+
 user_blueprint = Blueprint(name='user', import_name=__name__)
 
 
 @user_blueprint.route('/registration', methods=['POST'], endpoint='user_registration')
+@response_format
 def user_registration():
-    pass
+    body = dict(request.json)
+
+    user = UserService(body=body)
+
+    if not user.validate_body():
+        return RESPONSE_CODE[400], None
+
+    if not user.validate_name():
+        return RESPONSE_CODE[800], None
+
+    if not user.validate_nick_name():
+        return RESPONSE_CODE[801], None
+
+    if not user.validate_email():
+        return RESPONSE_CODE[802], None
+
+    if not user.validate_phone():
+        return RESPONSE_CODE[803], None
+
+    if not user.validate_password():
+        return RESPONSE_CODE[804], None
+
+    if not user.create_user():
+        return RESPONSE_CODE[400], None
+
+    return RESPONSE_CODE[200], None
 
 
 @user_blueprint.route('/login', methods=['POST'], endpoint='user_login')
 def user_login():
-    pass
+
+    return RESPONSE_CODE[200], None
 
 
 @user_blueprint.route('/logout', methods=['POST'], endpoint='user_logout')
 def user_logout():
-    pass
+    return RESPONSE_CODE[200], None
 
 
 @user_blueprint.route('/<int:user_id>/detail', methods=['GET'], endpoint='user_detail')
 def user_detail():
-    pass
+    return RESPONSE_CODE[200], None
 
 
 @user_blueprint.route('/details', methods=['GET'], endpoint='user_details')
 def user_details():
-    pass
+    return RESPONSE_CODE[200], None
+
