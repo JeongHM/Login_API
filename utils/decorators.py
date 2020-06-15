afed5db76e3dd15d53d90a32c66919f819a4213e
JeongHM <kjhm0607@gmail.com>
2020-06-15 23:57:05 +0900

    [Refactor] 헤더값 확인 로직 추가 #12, 응답객체 메모리에 남는 문제 해결

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,5 +1,8 @@
+import os
 import json
 from flask import request, Response
+
+from copy import deepcopy
 from utils.response_code import RESPONSE_CODE
 
 
@@ -8,10 +11,23 @@ def response_format(func):
         res, result = func(*args, **kwargs)
 
         if result:
-            res['result'] = result
+            res_copy = deepcopy(res)
+            res_copy['result'] = result
+            res = res_copy
 
         resp = json.dumps(res, ensure_ascii=False, default=str).encode('utf-8')
         return Response(response=resp, content_type='application/json; charset:utf-8')
+    return wrapper
+
+
+def header_required(func):
+    def wrapper(*args, **kwargs):
+        api_key = request.headers.get('Api-Key')
+
+        if not api_key and (api_key != os.getenv('API_KEY')):
+            return RESPONSE_CODE['MISSING_REQUIRED_HEADER']
+
+        return func(*args, **kwargs)
     return wrapper
 
 
@@ -20,6 +36,6 @@ def token_required(func):
         access_token = dict(request.headers).get('Access-Token')
 
         if not access_token:
-            return RESPONSE_CODE[401]
+            return RESPONSE_CODE['MISSING_TOKEN_HEADER']
         return func(*args, **kwargs)
     return wrapper
