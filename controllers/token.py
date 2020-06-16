c3cb034d17798149e45d6f923a5eec21bf01e141
JeongHM <kjhm0607@gmail.com>
2020-06-16 23:53:47 +0900

    [feat] 토큰 관련 컨트롤러 초기 구성 #20
    - validation과 refresh API 구성

--- controllers/token.py
+++ controllers/token.py
@@ -1,8 +1,22 @@
 from flask import Blueprint, request
 
+from utils.decorators import token_required, header_required, response_format
+from utils.response_code import RESPONSE_CODE
+
 token_blueprint = Blueprint(name='token', import_name=__name__)
 
 
+@token_blueprint.route('/validate', methods=['POST'], endpoint='token_validate')
+@header_required
+@token_required
+@response_format
+def token_validate():
+    return RESPONSE_CODE['SUCCESS'], None
+
+
 @token_blueprint.route('/refresh', methods=['POST'], endpoint='token_refresh')
+@header_required
+@token_required
+@response_format
 def token_refresh():
-    return 'hello world'
+    return RESPONSE_CODE['SUCCESS'], None
